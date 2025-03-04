04c1c600bf2380dea4e52bf7c939081c6d187a92
MITHUN MARTIN <mithun.martin.work@gmail.com>
2025-03-05 01:35:23 +0530

    Create q1.cpp

---
+++ Hard/q1.cpp
@@ -0,0 +1,21 @@
+// Question: Find the factorial of a number
+// Given an integer N, write a function to compute the factorial of N (N!).
+
+#include <iostream>
+using namespace std;
+
+// Function to calculate factorial
+int factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    
+    cout << "Factorial of " << num << " is " << factorial(num) << endl;
+    return 0;
+}
